b274825eb6272e4617f65357f9f453d978cde0ea
Chr15t1an <ccampbell@sapphirebd.com>
2019-10-25 10:23:01 -0500

    Update Vue Component.
    :+1:

--- card.vue
+++ card.vue
@@ -3,6 +3,21 @@
   <div class="custom-code-sample">
     <v-card>
       <v-card-text>
+        <p> {{trigger_name}} Results!</p>
+        <p v-if="totalUserscore < 3">You probably experience lots of stress, bouncing back and forth between Gamer attempts to keep your job and frustration with your staff.</p>
+        <p v-else-if="totalUserscore == 3">You likely do try to achieve results, but with a high degree of frustration common to the User style. You may try to be liked, but it generally doesn't work.</p>
+        <p v-else-if="totalUserscore == 4 && numOfShadedAs < 1">You may identify with the Pleaser style, putting lots of effort into being liked and reacting to one crisis after anouther.</p>
+        <p v-else-if="totalUserscore == 4 && numOfShadedAs >= 1">You probably are a results-oriented manager with an opportunity to sustain your results as you connect with and invest in your people.</p>
+        <p v-else-if="totalUserscore == 5">You have Winning Well management instincts that likely get buried in day-to-day stress and personal frustrations.</p>
+        <p v-else-if="totalUserscore == 6 && numOfShadedAs <= 2">You may identify with the Pleaser style, putting lots of effort into being liked and reacting to one crisis after anouther with some focus on results.</p>
+        <p v-else-if="totalUserscore == 6 && numOfShadedAs >= 3">You likely have Winning Well instincts and a focus on results.</p>
+        <p v-else-if="totalUserscore == 7">You likely have Winning Well instincts and focus on relation-ships. </p>
+        <p v-else-if="totalUserscore == 8">You likely have strong Winning Well practices and may flex easily between results and relationships.</p>
+        <p v-else-if="totalUserscore > 8">You likely have strong Winning Well practices and emphasize relationships in your day-today work. </p>
+        <div v-else>
+          Hmm. This is puzzling. I had trouble calculating your score. It could be an error on my end.
+        </div>
+        <hr/>
         <h1>Test</h1>
         <h5>The "ctx" prop</h5>
         <p>The ctx prop in the sample script above is a JavaScript object that provides your custom code a bunch of context about the learner taking the lesson and their progress, as well information about the lesson itself. Also, we include some organizational information as well. </p>
@@ -42,7 +57,7 @@
   module.exports = {
 
     //give your custom code component a unique name, no spaces, camelCase
-    name : 'CustomCodeSample',
+    name : 'CustomWw',
 
     //props are properties passed from the parent lesson context
     //into your component. Please always include this line
@@ -56,8 +71,9 @@
     //return here can be used in your templates like this Hi {{fullName}}
     data(){
       return {
-        ctxHidden : true,
-        email:this.ctx.learner.email;
+        trigger_name: this.ctx.lesson.name,
+        email:this.ctx.learner.email,
+        totalUserscore:8,
         fullName : [
           this.ctx.learner.first_name,
           this.ctx.learner.first_name
@@ -75,7 +91,7 @@
     //this attaches the parent lesson ui components to your custom code
     //so that you can use built in lesson vue components in YOUR code!
     beforeMount(){
-      this.$options.components = this.ui
+      this.$options.components = this.ui;
     },
 
     //methods are actions that occur as a result of clicking on items
@@ -86,11 +102,31 @@
       //the power to signal when the learner is completed you can control the learner's
       //ability to progress through the rest of the lesson!
       complete(){
-        this.$emit('completed')
+        this.$emit('completed');
       }
     },
 
     computed: {
+      totalUserscore: function () {
+        // loop through all items ctx.lesson.items
+        // a.lesson.items;
+        a = this.ctx;
+        var points = Number(0);
+        for (i = 0; i < a.lesson.items.length; i++) {
+            // console.log(a.lesson.items[i]);
+            item = a.lesson.items[i];
+            points = points + Number(item.engagementState.points);
+            // Count shaded twice.
+            meta = item.engagementState.meta;
+
+          }
+        // add all of the answes together
+        ///
+        ///
+
+        this.totalUserscore = points;
+      },
+
       learner(){
         return this.ctx.learner
       }
@@ -99,6 +135,12 @@
     //DO NOT REMOVE. This emits to the parent lesson that your custom code is fully loaded
     mounted(){
       this.$emit('loaded')
+      var data = {};
+      data['wwscore'] = this.totalUserscore;
+           Conveyour.identify(this.email, data).done(function(data){
+                 console.log(data)
+               });
+
     },
 
   }
---
+++ example.js
@@ -0,0 +1,451 @@
+var a ={
+  "startTime": "2019-10-25T14:38:05.901Z",
+  "endTime": null,
+  "duration": 192,
+  "lessonStats": {
+    "points": "010101111",
+    "completedItems": 8,
+    "unfinishedItems": 1,
+    "completed": false,
+    "completedPerc": 89
+  },
+  "progressStats": {
+    "points": "010101111",
+    "completedItems": 8,
+    "unfinishedItems": 0,
+    "completed": true,
+    "completedPerc": 100
+  },
+  "learner": {
+    "photo": "https://throwbocdn.s3.amazonaws.com/conveyour/ui/mary-mctesterson.jpg",
+    "meta": {
+      "tester": true
+    },
+    "first_name": "Mary",
+    "last_name": "McTesterson",
+    "email": "mary.mctesterson@example.com",
+    "mobile": "",
+    "alerts": "",
+    "id": "5d9b5a90c96a88906032f2da",
+    "points": 0,
+    "org_styles": {
+      "theme": "light",
+      "primary": "",
+      "secondary": "",
+      "headers_font": "Lobster"
+    }
+  },
+  "lesson": {
+    "name": "Winning well self assessment.",
+    "items": [
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Putting out fires",
+              "points": "1"
+            },
+            {
+              "answer": "Working long hours",
+              "points": "0"
+            }
+          ],
+          "metadata": {}
+        },
+        "editing": false,
+        "uuid": "90822550-f730-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Putting out fires",
+              "points": "1"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Putting out fires",
+            "points": "1"
+          },
+          "correct": false,
+          "points": "1",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Solving problems",
+              "points": "1"
+            },
+            {
+              "answer": "Knowing things before anyone else",
+              "points": "0"
+            }
+          ],
+          "metadata": {
+            "shaded": "true"
+          }
+        },
+        "editing": false,
+        "uuid": "a3f19440-f730-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Knowing things before anyone else",
+              "points": "0"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Knowing things before anyone else",
+            "points": "0"
+          },
+          "correct": false,
+          "points": "0",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Being recognized",
+              "points": "1"
+            },
+            {
+              "answer": "Building my status",
+              "points": "0"
+            }
+          ],
+          "metadata": {}
+        },
+        "editing": false,
+        "uuid": "e09a4bd0-f730-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Being recognized",
+              "points": "1"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Being recognized",
+            "points": "1"
+          },
+          "correct": false,
+          "points": "1",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Getting results",
+              "points": "1"
+            },
+            {
+              "answer": "Being confident",
+              "points": "0"
+            }
+          ],
+          "metadata": {
+            "shaded": "true"
+          }
+        },
+        "editing": false,
+        "uuid": "01b0e860-f731-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Being confident",
+              "points": "0"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Being confident",
+            "points": "0"
+          },
+          "correct": false,
+          "points": "0",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Being liked by my employees",
+              "points": "1"
+            },
+            {
+              "answer": "Achieving day-to-day goals",
+              "points": "0"
+            }
+          ],
+          "metadata": {}
+        },
+        "editing": false,
+        "uuid": "31617700-f731-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Being liked by my employees",
+              "points": "1"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Being liked by my employees",
+            "points": "1"
+          },
+          "correct": false,
+          "points": "1",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Working toward long-range goals",
+              "points": "1"
+            },
+            {
+              "answer": "Keeping my job",
+              "points": "0"
+            }
+          ],
+          "metadata": {
+            "shadded": "true"
+          }
+        },
+        "editing": false,
+        "uuid": "5252d850-f731-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Working toward long-range goals",
+              "points": "1"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Working toward long-range goals",
+            "points": "1"
+          },
+          "correct": false,
+          "points": "1",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Dealing with job-related stress",
+              "points": "1"
+            },
+            {
+              "answer": "Not feeling frustrated",
+              "points": "0"
+            }
+          ],
+          "metadata": {}
+        },
+        "editing": false,
+        "uuid": "d2f72060-f731-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Dealing with job-related stress",
+              "points": "1"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Dealing with job-related stress",
+            "points": "1"
+          },
+          "correct": false,
+          "points": "1",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "assessment",
+        "values": {
+          "question": "WHICH IS MORE IMPORTANT?",
+          "include_points": true,
+          "hide_results": false,
+          "answers": [
+            {
+              "answer": "Being respected",
+              "points": "1"
+            },
+            {
+              "answer": "Playing politics well",
+              "points": "0"
+            }
+          ],
+          "metadata": {
+            "shadded": "true"
+          }
+        },
+        "editing": false,
+        "uuid": "fbf004a0-f731-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [
+            {
+              "answer": "Being respected",
+              "points": "1"
+            }
+          ],
+          "completed": true,
+          "lastAnswer": {
+            "answer": "Being respected",
+            "points": "1"
+          },
+          "correct": false,
+          "points": "1",
+          "meta": {
+            "stats": null
+          }
+        },
+        "$first": true,
+        "$last": true
+      },
+      {
+        "type": "custom",
+        "values": {
+          "url": "https://gist.githubusercontent.com/srhyne/71d8687e27ef5e5e088d6986bb434050/raw/lesson-custom-code.vue",
+          "wrong_answer": 0,
+          "answer_points": 0,
+          "multiplier": 0,
+          "max": 0,
+          "metadata": {}
+        },
+        "editing": true,
+        "uuid": "3e8c7a50-f732-11e9-ba3a-8f6316efdea5",
+        "engagementState": {
+          "answers": [],
+          "completed": false,
+          "lastAnswer": null,
+          "correct": false,
+          "points": null,
+          "meta": null
+        },
+        "$first": true,
+        "$last": true
+      }
+    ],
+    "folder_id": "5db3010ec96a885972cedf09",
+    "uuid": "7e026d90-f730-11e9-ba3a-8f6316efdea5",
+    "isDirty": false,
+    "user_id": 989,
+    "org_id": 504,
+    "updated_at": 1572013115,
+    "created_at": 1572012338,
+    "etl": false,
+    "id": "5db30132c96a88e670cedf0a",
+    "items_count": 9,
+    "length": "4 min, 30 sec",
+    "banner_img": "",
+    "icon": "guage"
+  },
+  "org": {
+    "org_id": "504",
+    "pin_required": false,
+    "native_app": true,
+    "portal_allow_missing_info": true,
+    "portal_css": "",
+    "features": {
+      "lessons": true
+    },
+    "portal_path": "v2"
+  }
+}
+
+//Get user email
+
+//#Get User Points
+ctx.lessonStats.points
+
+
+
+//Save the user data in the contact profile
+data['wwscore'] = this.totalScore;
+            Conveyour.identify(this.userEmail, data).done(function(data){
+                  console.log(data)
+                });
+              console.log('Test');
+
+
+
+// get assessment triggers
+
+ctx.lesson.items
+
+
+
+// User Score is total score + shadded
+//  add the score for shaded values twice.
