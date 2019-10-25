4f5b0cac6e044a57de2c43f8333fec92994cf61b
Chr15t1an <ccampbell@sapphirebd.com>
2019-10-25 15:06:00 -0500

    Update card.vue
    Attempting to get the identification to work.

--- card.vue
+++ card.vue
@@ -3,8 +3,10 @@
   <div class="custom-code-sample">
     <v-card>
       <v-card-text>
-        <p> {{trigger_name}} Results!</p>
-        <p>Total User Score => {{totalUserscore}}</p>
+        <h2> {{trigger_name}} Results!</h2>
+        <p>Total Score => {{totalUserscore}}</p>
+        <p>Number of Shaded A's => {{numOfShadedAs}}</p>
+
         <p v-if="totalUserscore < 3">You probably experience lots of stress, bouncing back and forth between Gamer attempts to keep your job and frustration with your staff.</p>
         <p v-else-if="totalUserscore == 3">You likely do try to achieve results, but with a high degree of frustration common to the User style. You may try to be liked, but it generally doesn't work.</p>
         <p v-else-if="totalUserscore == 4 && numOfShadedAs < 1">You may identify with the Pleaser style, putting lots of effort into being liked and reacting to one crisis after anouther.</p>
@@ -19,14 +21,8 @@
           Hmm. This is puzzling. I had trouble calculating your score. It could be an error on my end.
         </div>
         <hr/>
-        <h1>Test</h1>
-        <v-btn @click="ctxHidden = !ctxHidden" color="primary">
-          <span v-if="ctxHidden">View ctx property values</span>
-          <span v-else>Hide ctx values</span>
-        </v-btn>
-        <pre v-if="!ctxHidden">{{ctx}}</pre>
 
-        <h5 style="margin-top: 20px">Completion</h5>
+        <h5 style="margin-top: 20px">Mark Complete</h5>
         <v-btn color="primary" @click="complete">complete</v-btn>
       </v-card-text>
     </v-card>
@@ -43,14 +39,11 @@
 <!-- put your javascript / vue.js logic into your script tag -->
 <!-- lang='es6' is important. Please include -->
 <script lang='es6'>
-
-//
   //your code should be exported using the module.exports object
   module.exports = {
 
     //give your custom code component a unique name, no spaces, camelCase
     name : 'CustomWw',
-
     //props are properties passed from the parent lesson context
     //into your component. Please always include this line
     //@ctx = the lesson context object
@@ -63,17 +56,17 @@
     //return here can be used in your templates like this Hi {{fullName}}
     data(){
       return {
-        ctxHidden : true,
         trigger_name: this.ctx.lesson.name,
         email:this.ctx.learner.email,
-        totalUserscore:8,
-        fullName : [
-          this.ctx.learner.first_name,
-          this.ctx.learner.first_name
-        ].join(' ')
+        score:0,
+        domain:this.ctx.lesson.metadata[0].value,//this.ctx.lesson.metadata['domain'],
+        appkey:this.ctx.lesson.metadata[1].value,//this.ctx.lesson.metadata['appkey'],
+        token:this.ctx.lesson.metadata[2].value,//this.ctx.lesson.metadata['token'],
       }
     },
     created: function() {
+
+
 
     },
 
@@ -81,6 +74,7 @@
     //so that you can use built in lesson vue components in YOUR code!
     beforeMount(){
       this.$options.components = this.ui;
+
     },
 
     //methods are actions that occur as a result of clicking on items
@@ -96,57 +90,110 @@
     },
 
     computed: {
-      totalUserscore: function () {
-        // loop through all items ctx.lesson.items
-        // a.lesson.items;
-        a = this.ctx;
+      learner(){
+        return this.ctx.learner;
+      },
+      totalUserscore(){
+        // store values in var
+        var a = this.ctx;
         var points = Number(0);
-        for (i = 0; i < a.lesson.items.length; i++) {
+        var numOfShadedAs = this.numOfShadedAs;
+        for (var i = 0; i < a.lesson.items.length; i++) {
             // console.log(a.lesson.items[i]);
-            item = a.lesson.items[i];
+            var item = a.lesson.items[i];
             points = points + Number(item.engagementState.points);
+          }
+        var totalpoints = points+numOfShadedAs;
+        self.score = totalpoints;
+
+
+
+        console.log('identify');
+        var emaiL = this.email;
+        var data = {};
+        data['wwscore1'] = Number(totalpoints);
+
+        // setTimeout(function(){
+        Conveyour.identify(emaiL, data).done(function(data){
+                   console.log(data)
+                 });
+
+
+
+
+
+        return totalpoints;
+      },
+      numOfShadedAs(){
+        var a = this.ctx;
+        var points = Number(0);
+        var numOfShadedAs = Number(0);
+
+        for (var i = 0; i < a.lesson.items.length; i++) {
+            // console.log(a.lesson.items[i]);
+            var item = a.lesson.items[i];
+            // points = points + Number(item.engagementState.points);
             // Count shaded twice.
-            meta = item.values.metadata;
+            var meta = item.values.metadata;
             if (meta['shaded']) {
-              points = points + Number(item.engagementState.points);
+              numOfShadedAs = numOfShadedAs + Number(item.engagementState.points);
             }
 
           }
-
-        this.totalUserscore = points;
+        return numOfShadedAs;
       },
-
-      learner(){
-        return this.ctx.learner;
-      }
     },
 
     //DO NOT REMOVE. This emits to the parent lesson that your custom code is fully loaded
     mounted(){
-      this.$emit('loaded')
+      this.$emit('loaded');
+
+
+      // var jQuery = $;
+
+      // console.log(this.domain);
+
+
+      var domaiN = this.domain;
+      var appkeY = this.appkey;
+      var tokeN = this.token;
+      // var totalUserscorE = this.totalUserscore;
+      // var emaiL = this.email;
+
+      document.readyState
+
+
+      var callback = function(){
+  // Handler when the DOM is fully loaded
+
+      console.log('ran');
       (function(g) {
             var ns, script, first;
             ns = 'Conveyour';
             g[ns] = g[ns] || {};
             g[ns].config = {
-              domain: this.ctx.lesson.metadata['domain'],
-              appkey: this.ctx.lesson.metadata['appkey'],
-              token:  this.ctx.lesson.metadata['token']
+              domain: domaiN,//this.ctx.lesson.metadata['domain'],
+              appkey: appkeY,//this.ctx.lesson.metadata['appkey'],
+              token: tokeN,// this.ctx.lesson.metadata['token']
             };
             g[ns].identify = function() { g[ns].q = g[ns].q || []; g[ns].q.push({m: 'identify', as: arguments}); };
             g[ns].track = function() { g[ns].q = g[ns].q || []; g[ns].q.push({m: 'track', as: arguments}); };
             script = document.createElement('script');
             script.type = 'text/javascript';
             script.async = true;
+            // script.async = false;
             script.src = 'https://app.conveyour.com/js/cdn/analytics.v1.min.js';
             first = document.getElementsByTagName('script')[0];
             first.parentNode.insertBefore(script, first);
           })(window);
-      // var data = {};
-      data['wwscore'] = this.totalUserscore;
-           Conveyour.identify(this.email, data).done(function(data){
-                 console.log(data)
-               });
+
+        };
+        if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+          callback();
+        } else {
+          document.addEventListener("DOMContentLoaded", callback);
+        }
+
 
     },
 
